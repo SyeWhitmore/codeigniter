a50fb2770bed716127e1dd5f839057f5edc66e98
Simon Whitmore <simonwhitmore@Simons-MacBook-Pro.local>
2018-02-15 14:56:39 +0000

    bootstrap setup

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -6,6 +6,7 @@ class pages extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('layout/header');
+		$this->load->view('layout/navigation');
 		$this->load->view('content/home');
 		$this->load->view('layout/footer');
 	}
